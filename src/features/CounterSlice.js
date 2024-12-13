8c0f7c14560ef7ba7c4febf645dd3e01df35f77b
Mohsen Deilami <mohsen.deilami6570@gmail.com>
2024-12-13 23:48:42 +0100

    create slice

--- src/features/CounterSlice.js
+++ src/features/CounterSlice.js
@@ -1,1 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
+
+const initialState={
+    countValue:0,
+}
+
+const counterSlice=createSlice({
+    name:'Counter',
+    initialState,
+    reducer:{
+        increment:(state, action)=>{
+            return state.countValue ++
+        },
+        decremnt:(state , action)=>{
+            return statet.countValue --
+        }
+    }
+})
+export default counterSlice.reducer;
+export const {decremnt , increment} = counterSlice.actions;
