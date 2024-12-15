93412c25b1e15e1161296697a00fe594fc8bda3b
Mohsen Deilami <mohsen.deilami6570@gmail.com>
2024-12-15 10:19:49 +0100

    payload

--- src/Component/Counter.jsx
+++ src/Component/Counter.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector , useDispatch } from 'react-redux';
 import { selectCounter} from '../features/CounterSlice';
 import {increment , decremnt , incrementByAmount} from './../features/CounterSlice';
@@ -8,13 +8,17 @@ import {increment , decremnt , incrementByAmount} from './../features/CounterSli
 export default function Counter() {
   const counter=useSelector(selectCounter)
   const dispatch=useDispatch();
- 
+ const [payloadValue ,  setPayloadValue] =useState(2)
   return (
     <div>
       <p>{counter}</p>
+
+      <input type="number" value={payloadValue} onChange={event => setPayloadValue(event.target.value)} />
+      <br />
+      <br />
      <button onClick={()=>dispatch(increment())}>Add</button>
      <button onClick={()=>dispatch(decremnt())}>Minus</button>
-     <button onClick={()=>dispatch(incrementByAmount(2))}>Increment By Amount</button>
+     <button onClick={()=>dispatch(incrementByAmount(+payloadValue))}>Increment By Amount</button>
     </div>
   )
 }
